cc992b3ebc28cb72f5825977cb0d6eca3e8c44ac
shyzhao <zhao119829@163.com>
2022-04-19 21:50:48 +0800

    定义类和属性

--- tmptest/class_test/object02.py
+++ tmptest/class_test/object02.py
@@ -15,13 +15,13 @@ class Student:
 
 xiao = Student()
 xiao.gender = 'male'  # 对象属性
-xiao.age = 24
+xiao.age = 24  # 对象属性
 print(xiao.gender)
 print(xiao.age)
 
 xx = Student()
-print('xx.age = ', xx.age)
+print('xx.age = ', xx.age)  # 此处xx对象未创建属性，就会去类中寻找相关属性
 
-Student.age = 10
-print('xx.age = ', xx.age)
-print('xiao.age = ', xiao.age)
+Student.age = 10  # 此处修改了类的属性值
+print('xx.age = ', xx.age)  # 此处返回的是类中的属性值
+print('xiao.age = ', xiao.age)  # xiao对象中有age属性，故此处返回的是xiao对象中age属性的值
