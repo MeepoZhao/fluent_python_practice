e2d997658a8d89c98be17254ace063bac67164e6
shyzhao <zhao119829@163.com>
2022-04-20 21:46:32 +0800

    类的方法中self定义

--- tmptest/class_test/object03.py
+++ tmptest/class_test/object03.py
@@ -9,12 +9,12 @@
 
 
 class Phone:
-    brand = 'xiaomi'
+    brand = 'xiaomi'  # 类的属性
 
     # price = 3999
 
     def __init__(self, price: int) -> None:  # 初始化动作，创建对象时会执行
-        self.price = price
+        self.price = price  # 对象的属性
 
     def call(self):
         print('self-------->', self)
@@ -29,3 +29,8 @@ print(phone1)
 phone1.call()  # call(phone1)，此处类中函数定义的self指的是phone1
 phone1.price = 5000
 phone1.get_price()
+phone1.brand = 'iphone'  # 将属性brand动态绑定到phone1对象上
+print(phone1.brand)
+phone2 = Phone(6777)
+print(phone2.brand)
+
