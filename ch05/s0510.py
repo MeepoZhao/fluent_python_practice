1708a78baea4abbf48250f78479ea7898394c997
MeepoZhao <zhao119829@163.com>
2022-04-20 10:57:02 +0800

    位置参数和参数收集——生成一个或多个HTML标签

--- ch05/s0510.py
+++ ch05/s0510.py
@@ -19,6 +19,18 @@ def tag(name, *content, cls=None, **attrs):
         return '<%s%s />' % (name, attr_str)
 
 
+def f(a: int, *, b: int) -> tuple:
+    """
+    定义函数时，若想指定仅限关键字参数，要把他们放在前面有*的参数后面。
+    如果不想支持数量不定的定位参数，但是想支持仅限关键字参数，在签名中放一个*
+    仅限关键字参数不一定要有默认值，可以按照该函数定义的方式，强制必须传入实参
+    :param a:
+    :param b:
+    :return:
+    """
+    return a, b
+
+
 if __name__ == '__main__':
     # print(tag('br'))  # name = 'br'
     # print(tag('p', 'hello'))  # name = 'p',content = ['hello']
@@ -26,3 +38,6 @@ if __name__ == '__main__':
     # print(tag('p', 'hello', id=33))  # name = 'p',content = ['hello'],attrs = {'id':33}
     # print(tag('p', 'hello', 'world', cls='sidebar'))  # name='p',content=['hello','world'],cls='sidebar'
     print(tag(content='testing', name='img'))  # name='img',attrs={'content':'testing'}
+    my_tag = {'name': 'img', 'title': 'Sunset Boulevard', 'src': 'sunset.jpg', 'cls': 'framed'}
+    print(tag(**my_tag))  # 字典中所有元素作为单个参数传入，同名的键会绑定到对应的具名参数上，余下的会被attrs捕获
+    print(f(1, b=2))  # 调用f(1, 2)会报错：f() takes 1 positional argument but 2 were given
